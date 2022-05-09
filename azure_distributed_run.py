4482050a9af146f87cb58aa52655f1296ec9562d
BUAAw-ML <wangqb6@outlook.com>
2022-05-09 20:52:08 +0800

    up

--- azure_distributed_run.py
+++ azure_distributed_run.py
@@ -10,7 +10,7 @@ from meter.datamodules.multitask_datamodule import MTDataModule
 
 import resource
 rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
-resource.setrlimit(resource.RLIMIT_NOFILE, (20480, rlimit[1]))
+# resource.setrlimit(resource.RLIMIT_NOFILE, (20480, rlimit[1]))
 
 from pytorch_lightning.plugins.environments import ClusterEnvironment
 from pytorch_lightning.plugins.training_type import DDPPlugin
